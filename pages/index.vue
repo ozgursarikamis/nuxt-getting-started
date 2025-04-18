fd4e92d845c3b4013f3489a54d1b9a9ddd5f8d1a
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-04-19 00:52:00 +0300

    NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
+        <div>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/products">Products</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
     <h1>Page Index</h1>
 </template>
