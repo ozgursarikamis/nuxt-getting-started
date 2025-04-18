abfd72b8586cbd7cb25d77bf908786ae79dc20ce
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-04-19 01:00:39 +0300

    Layouts

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/products">Products</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- output the page content -->
+     <div>
+        <slot />
+     </div>
+</template>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
