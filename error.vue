d7dd0047df4e4ae4a39e1203bc715b1ec06f9504
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-04-19 23:13:38 +0300

    `clearError()`

--- error.vue
+++ error.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
         <UAlert color="neutral" :title="error?.message" />
+        <br />
+        <UButton color="neutral" @click="handleClearError">Go home</UButton>
     </div>
 </template>
 
 <script setup>
-    defineProps(['error'])
+defineProps(['error']);
+
+const handleClearError = () => clearError({ redirect: '/' })
 </script>
