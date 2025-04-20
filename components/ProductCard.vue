2c89fd8a4386bdfad696f233eee8996b629a31f9
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-04-20 13:43:59 +0300

    product images on card

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,18 +1,16 @@
 <template>
-  <UDrawer direction="right" inset>
+  <!-- <UDrawer direction="right" inset>
     <UButton label="Open" color="neutral" variant="subtle" trailing-icon="i-lucide-chevron-up"/>
 
     <template #content>
-      Card Content
+      Drawer content
     </template>
-  </UDrawer>
-  <UCard variant="soft">
+  </UDrawer> -->
+  <UCard variant="soft" style="margin-bottom: 1rem;">
     <template #header>
       <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
     </template>
-
-    [Content]
-
+    <img :src="product.image" alt="product.title" style="max-height: 4rem;"/>
     <template #footer>
       Footer Content
     </template>
