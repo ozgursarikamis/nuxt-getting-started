2928d73ed2db8a8994bca933e33acc605ab00361
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-04-26 18:20:10 +0300

    counter store in component

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
 <template>
-  <NuxtWelcome />
+  {{ store.count }}
+
+  <button @click="increment()">+ Increment</button>
+  <button @click="decrement()">- Decrement</button>
 </template>
+
+<script setup>
+
+import { useCounterStore } from './stores/counter';
+
+console.log({
+  message: 'Hello from app.vue'
+})
+
+const store = useCounterStore();
+
+const increment = () => {
+  store.increment();
+}
+
+const decrement = () => {
+  store.decrement();
+}
+</script>
--- stores/counter.ts
+++ stores/counter.ts
@@ -1,14 +1,17 @@
 import { defineStore } from 'pinia';
 
-export const useCounterStore = defineStore('counter', {
-    state: () => ({ count: 0 }),
-    getters: { // selectors
-        doubleCount: (state) => state.count * 2,
-        tripleCount: (state) => state.count * 3,
-    },
-    actions: {
-        increment() {
-            this.count++;
-        }
+export const useCounterStore = defineStore('counter', () => {
+    const count = ref(0);
+
+    const doubleCount = computed(() => count.value * 2)
+
+    function increment() {
+        count.value++;
     }
-});
+
+    function decrement() {
+        count.value--;
+    }
+
+    return { count, doubleCount, increment, decrement }
+})
