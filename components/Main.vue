6847cd5ee0b801899e296f52626da7894a496231
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-09-02 19:30:46 +0300

    Form elements

--- components/Main.vue
+++ components/Main.vue
@@ -1,8 +1,37 @@
-<script></script>
+<script setup lang="ts">
+import type { RadioGroupItem, RadioGroupValue } from '@nuxt/ui'
+import type { CheckboxGroupItem } from '@nuxt/ui';
+
+const checkboxGroupItem = ref<CheckboxGroupItem[]>(['System', 'Light', 'Dark'])
+const checkboxValue = ref(true);
+
+const radioGroupItems = ref<RadioGroupItem[]>([
+    {
+        label: 'System',
+        description: 'This is the first option.',
+        value: 'system'
+    },
+    {
+        label: 'Light',
+        description: 'This is the second option.',
+        value: 'light'
+    },
+    {
+        label: 'Dark',
+        description: 'This is the third option.',
+        value: 'dark'
+    }
+]);
+
+const value = ref<RadioGroupValue>('system');
+
+</script>
 <template>
-<div>
-    Main
-</div>
+    <div>
+        <UCheckbox v-model="checkboxValue" label="Checkbox 1" />
+        <UCheckboxGroup indicator="end" variant="card" :default-value="['System']" :items="checkboxGroupItem" />
+        <URadioGroup v-model="value" :items="radioGroupItems" />
+    </div>
 </template>
 
 <style scoped>
