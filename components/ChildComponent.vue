0f518dbc8e5a3465a13cc8fcf1f0c05cb7185be7
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-09-02 14:59:00 +0300

    `provide` and `inject` methods

--- components/ChildComponent.vue
+++ components/ChildComponent.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+// To inject data provided by an ancestor component, use the inject() function:
+
+import { inject } from 'vue';
+
+const foo = inject('foo');
+
+</script>
+
 <template>
     <div>
         Child Component
     </div>
+    Reading from parent: {{ foo }}
 </template>
--- components/ParentComponent.vue
+++ components/ParentComponent.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+// To provide data to a component's descendants, use the provide() function:
+
+import { provide } from 'vue';
+
+provide('foo', 'bar 1234');
+
+</script>
 <template>
     <div>
         Parent Component
