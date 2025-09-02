9cd246f3eb42e551e964f74d2fdfecb9f003683a
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-09-02 19:55:06 +0300

    watch method for watching changes

--- components/Main.vue
+++ components/Main.vue
@@ -1,7 +1,22 @@
-<script></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const count = ref(0);
+const doubleCount = computed(() => count.value * 2);
+
+setTimeout(() => {
+    count.value = Math.random();
+}, 1500);
+
+// watch for changes for count:
+watch(count, (newCount) => {
+    console.log('count changed', newCount);
+});
+</script>
 <template>
 <div>
-    Main
+    Count: {{  count }} <br>
+    Double Count: {{ doubleCount }}
 </div>
 </template>
 
