56449f06c87093cd2af8037a92d7a8fd37f3fa91
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-09-02 20:00:15 +0300

    Watching for multiple resources' changes at once

--- components/Main.vue
+++ components/Main.vue
@@ -12,16 +12,36 @@ setTimeout(() => {
 watch(count, (newCount) => {
     console.log('count changed', newCount);
 });
+
+const firstName = ref<string>('');
+const lastName = ref<string>('');
+
+watch([firstName, lastName], ([newFirstName, newLastName], [oldFirstName, oldLastName]) => {
+    console.log(`First name: ${oldFirstName} → ${newFirstName}`)
+    console.log(`Last name: ${oldLastName} → ${newLastName}`)
+});
+
+function submit() {
+    console.log('submitted', firstName.value, lastName.value);
+}
+
 </script>
 <template>
-<div>
-    Count: {{  count }} <br>
-    Double Count: {{ doubleCount }}
-</div>
+    <div>
+        Count: {{ count }} <br>
+        Double Count: {{ doubleCount }}
+    </div>
+    <hr>
+    <div>
+        <br>
+        <UInput v-model="firstName" placeholder="First name" />
+        <UInput v-model="lastName" placeholder="Last name" />
+        <UButton @click="submit">Submit</UButton>
+        <br>
+        <br>
+        <hr>
+        {{ firstName }} {{ lastName }}
+    </div>
 </template>
 
-<style scoped>
-* {
-    padding: 1rem;
-}
-</style>
+<style scoped></style>
