4c2b80c9795d76eca8634ab289d23035ba2f400b
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-09-08 11:29:38 +0300

    [REFACTOR]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,28 @@ import { link } from "#build/ui";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devServer: {
-    port: 4200
-  }, devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/ui'
-  ],
-  css: ['~/assets/css/main.css'],
-  app: {
-    head: {
-      link: [
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap' }
-      ]
+    compatibilityDate: '2024-11-01',
+    devServer: {
+        port: 4200
+    },
+    devtools: { enabled: true },
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/fonts',
+        '@nuxt/icon',
+        '@nuxt/image',
+        '@nuxt/scripts',
+        '@nuxt/ui'
+    ],
+    css: ['~/assets/css/main.css'],
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap'
+                }
+            ]
+        }
     }
-  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,26 @@
 <template>
-    <div>
-        <header>
-            <Header />
-        </header>
-        <main>
-            <Main />
-        </main>
-        <footer>
-            <Footer />
-        </footer>
-    </div>
+  <div>
+    <header>
+      <Header/>
+    </header>
+    <main>
+      <Main/>
+    </main>
+    <footer>
+      <Footer/>
+    </footer>
+  </div>
 </template>
 
 <style>
 footer {
-    margin-top: 3rem;
-    padding: 1rem;
-    background-color: #eee;
-    text-align: center;
-    text-align: center;
-    border-top: 1px solid salmon;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
+  margin-top: 3rem;
+  padding: 1rem;
+  background-color: #eee;
+  text-align: center;
+  border-top: 1px solid salmon;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 }
 </style>
