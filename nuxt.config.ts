bb6bb6d414b79319b218249af7af295cc9955f24
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-09-22 13:24:31 +0300

    Sansation Font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
             link: [
                 {
                     rel: 'stylesheet',
-                    href: 'https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap'
+                    href: 'https://fonts.googleapis.com/css2?family=Sansation:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap'
                 }
             ]
         }
